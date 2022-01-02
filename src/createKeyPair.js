f1183c512ca68ea0673797023e45440958741105
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-01-02 16:00:53 -0600

    add createKeyPair

---
+++ src/createKeyPair.js
@@ -0,0 +1,9 @@
+// Props go to Nader Dabit for this one
+// https://twitter.com/dabit3
+
+const fs = require('fs')
+const anchor = require("@project-serum/anchor")
+
+const account = anchor.web3.Keypair.generate()
+
+fs.writeFileSync('./keypair.json', JSON.stringify(account))
